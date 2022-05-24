0691ff079dafa54b43e8622057662d92eac86caa
JoseQuintana20 <jquintanaf2001@gmail.com>
2022-05-23 21:25:50 -0500

    Se añade carpeta mu_code (PC)

--- DHT/ad.py
+++ DHT/ad.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 import time
+import json
 
 fecha = time.localtime()
 a = '{}/{}/{}'.format(fecha[2],fecha[1],fecha[0])
@@ -21,25 +21,34 @@ def calculo():
     return 1,2,3
 
 a,b,c=calculo()
-print(b)
-=======
-import time
+#print(b)
 
-fecha = time.localtime()
-a = '{}/{}/{}'.format(fecha[2],fecha[1],fecha[0])
-#a = fecha.fo    t(fecha[2],fecha[1],fecha[0])
-temp = 25
+string = "((26.1,),)"
+characters = "(,)"
 
-if temp > 27:
-    a = 1
-else:
-    a = 2 
+#for x in range(len(characters)):
+#    string = string.replace(characters[x],"")
+#s = int(string)
+#print(s)
 
-form = {
-    "email": str('jose'),
-    "password": str('1234'),
+variables = {
+    "fecha": time.localtime(),
+    "temp": 1,
+    "temp_f": 2,
 }
 
+fecha = variables['fecha']
+variables['fecha'] = '{}/{}/{}'.format(fecha[2],fecha[1],fecha[0])
+print(variables['fecha'])
+print(variables['temp'])
+print(variables['temp_f'])
+print("--------------------------------------------")
+print(fecha)
+print("--------------------------------------------")
+print("--------------------------------------------")
 
-print(form['email'])
->>>>>>> 254203cdecff57d8c0e2c3ba55fabc0b51bf8bca
+payload = json.dumps(variables)
+print(payload)
+print("--------------------------------------------")
+
+
